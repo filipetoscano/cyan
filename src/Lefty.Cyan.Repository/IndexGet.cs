ce98d675c78f3ece882d34d31f46c17b35b990b1
Filipe Toscano <filipe.toscano@gmail.com>
2026-03-26 12:50:46 +0400

    Fix sorting of directory users

--- src/Lefty.Cyan.Repository/IndexGet.cs
+++ src/Lefty.Cyan.Repository/IndexGet.cs
@@ -8,11 +8,11 @@ public partial class RepositoryService
         var idx = new Dictionary<string, List<string>>();
         var dir = new DirectoryInfo( Path.Combine( _config.Root, "people" ) );
 
-        foreach ( var corpDir in dir.GetDirectories() )
+        foreach ( var corpDir in dir.GetDirectories().OrderBy( x => x.Name ) )
         {
             var l = new List<string>();
 
-            foreach ( var personDir in corpDir.GetDirectories() )
+            foreach ( var personDir in corpDir.GetDirectories().OrderBy( x => x.Name ) )
                 l.Add( personDir.Name );
 
             idx.Add( corpDir.Name, l );
